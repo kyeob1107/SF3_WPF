ed695b75cfc6f4ddf9ed0e007628a3312d624779
kyeob1107 <koak1107@gmail.com>
2025-01-15 12:08:10 +0900

    scottPlot 계속 안되다가 4.1.68은 지금 나타나는 cookbook과는 다른 방식으로 사용한다는 것을 알고 참고해서 드디어 quick start에 있는 첫발 떼서 혹시몰라서 커밋

---
+++ scottPlot1/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ScottPlot;
+
+namespace scottPlot1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            // create sample data
+            double[] dataX = { 1, 2, 3, 4, 5 };
+            double[] dataY = { 1, 4, 9, 16, 25 };
+
+            // add a scatter plot to the plot
+            WpfPlot1.Plot.AddScatter(dataX, dataY);
+            WpfPlot1.Refresh();
+        }
+    }
+}
